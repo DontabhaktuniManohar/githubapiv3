3bcc72e139939939f7276409fff7a633a6f7a94c
DontabhaktuniManohar <dmanohar.murali@gmail.com>
2024-01-29 12:14:48 +0530

    Update jenkinsjob.groovy

--- jenkinsjob.groovy
+++ jenkinsjob.groovy
@@ -1,28 +1,32 @@
-def jobName = 'YourJobName'
+pipeline {
+    agent any
 
-def job = Jenkins.instance.getItemByFullName(jobName)
-if (job) {
-    def configXml = job.getConfigFile().getText()
-    
-    // Parse the XML to extract user names
-    def userNames = []
-    def xmlParser = new XmlParser()
-    def configData = xmlParser.parseText(configXml)
-    
-    // Assuming users are defined in <authorizationMatrix> section
-    def authorizationMatrix = configData['authorizationMatrix']
-    if (authorizationMatrix) {
-        authorizationMatrix['permission'].each { permission ->
-            if (permission["@class"] == 'hudson.security.ProjectMatrixAuthorizationStrategy$ProjectMatrixEntry') {
-                // Extract user names from permission definitions
-                permission['user'].each { user ->
-                    userNames.add(user.text())
+    stages {
+        stage('Extract Users List') {
+            steps {
+                script {
+                    def jobName = 'YourJobName'
+                    def configFile = readFile("${JENKINS_HOME}/jobs/${jobName}/config.xml")
+
+                    // Parse the config.xml content
+                    def configXml = new XmlSlurper().parseText(configFile)
+
+                    // Extract user names
+                    def userNames = []
+
+                    // Assuming users are defined in the <authorizationMatrix> section
+                    configXml.authorizationMatrix.each { permission ->
+                        if (permission["@class"] == 'hudson.security.ProjectMatrixAuthorizationStrategy$ProjectMatrixEntry') {
+                            // Extract user names from permission definitions
+                            permission.user.each { user ->
+                                userNames.add(user.text())
+                            }
+                        }
+                    }
+
+                    println "User names in $jobName job configuration: $userNames"
                 }
             }
         }
     }
-    
-    println "User names in $jobName job configuration: $userNames"
-} else {
-    println "Job '$jobName' not found."
 }
